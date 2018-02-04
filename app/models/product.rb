55f516b72bd2df1c624dfdc0515881d68461aa50
Tammy Callahan <trenec@me.com>
2018-02-04 17:13:58 -0600

    5.2 Added search form & method, reverted root page and redirect for new product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   has_many :orders
+  def self.search(search_term)
+    if Rails.env.production?
+      like_term = "ilike"
+    else
+      like_term = "LIKE"
+    end
+    Product.where("name #{like_term} ?", "%#{search_term}%")
+  end
 end
