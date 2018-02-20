66d9449bffa0778f9675e585159595239636992d
Tammy Callahan <trenec@me.com>
2018-02-20 13:42:00 -0600

    6.6 Added order creation and card error message

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -16,8 +16,16 @@ class PaymentsController < ApplicationController
         description: params[:stripeEmail],
         receipt_email: params[:stripeEmail]
       )
+
+      if charge.paid
+        Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
+        flash[:success] = "Thank you for your order! Your payment was processed successfully."
+      end
+
     rescue Stripe::CardError => e
-      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
 
     redirect_to products_url, notice: 'Thank you for your order! It was successfully processed.'
